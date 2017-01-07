98580a6519e60366063c18b7dbf3ec13d096c8fd
Tracyfoster <tracyfoster99@gmail.com>
2017-01-07 13:30:22 +0100

    Some changes to the tests

--- FizzBuzz/tests/test.js
+++ FizzBuzz/tests/test.js
@@ -38,6 +38,18 @@ describe("Fizz Buzz tests ", function() {
     expect(myApp.fizzBuzz(101)).toBe(101);
   });
 
+  it("should return 0 since its indivisible by 3 and 5", function() {
+    expect(myApp.fizzBuzz(0)).toBe(0);
+  });
+
+  it("should return -14 since its indivisible by 3 and 5", function() {
+    expect(myApp.fizzBuzz(-14)).toBe(-14);
+  });
+
+  it("should return `FizzBuzz` for -15", function() {
+    expect(myApp.fizzBuzz(-15)).toBe('FizzBuzz');
+  });
+
  });
 
 })();
--- MinMax/tests/test.js
+++ MinMax/tests/test.js
@@ -15,8 +15,24 @@ var myApp = require('../app/minMax.js');
         expect(myApp.findMinMax([6, 4])).toEqual([4, 6]);
       });
 
+      it('should return [-3, 20] for [-1, 20, -3, 4]', function () {
+        expect(myApp.findMinMax([-1, 20, -3, 4])).toEqual([-3, 20]);
+      });
+
       it('should return [2, 78] for [4, 66, 6, 44, 7, 78, 8, 68, 2]', function () {
         expect(myApp.findMinMax([4, 66, 6, 44, 7, 78, 8, 68, 2])).toEqual([2, 78]);
+      });
+      
+      it('should return [-50, 0] for [-10, -2, -50, 0]', function () {
+        expect(myApp.findMinMax([-10, -2, -50, 0])).toEqual([-50, 0]);
+      });
+
+      it('should return [-15, 40] for [40, -7, 1, -15, 0]', function () {
+        expect(myApp.findMinMax([40, -7, 1, -15, 0])).toEqual([-15, 40]);
+      });
+
+      it('should return [-5.9, 47] for [-1, -2, 47, 0,-1.05, -2, -5.9, 8]', function () {
+        expect(myApp.findMinMax([-1, -2, 47, 0,-1.05, -2, -5.9, 8])).toEqual([-5.9, 47]);
       });
 
     });
@@ -25,6 +41,14 @@ var myApp = require('../app/minMax.js');
 
       it('should return [4] for [4, 4, 4, 4]', function () {
         expect(myApp.findMinMax([4, 4, 4, 4])).toEqual([4]);
+      });
+
+      it('should return [-9] for [-9, -9]', function () {
+        expect(myApp.findMinMax([-9, -9])).toEqual([4]);
+      });
+
+      it('should return [0] for [0, 0, 0]]', function () {
+        expect(myApp.findMinMax([0, 0, 0])).toEqual([4]);
       });
 
     });
